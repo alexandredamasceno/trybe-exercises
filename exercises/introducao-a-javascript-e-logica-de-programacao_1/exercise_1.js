9954333148e0dd795e129ee945d6c9b70ee237f1
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-02-25 09:23:46 -0300

    Sétimo requisito

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_1.js
@@ -95,4 +95,24 @@ switch (peca) {
 
 //Aprendi a converter as palavras em minúsculas usando o .toLowerCase() no site:https://qastack.com.br/programming/154862/convert-javascript-string-to-be-all-lower-case
 
+console.log("Sétimo Requisito:")
+
+let nota = 100;
+
+if (nota >= 90) {
+    console.log("A");
+} else if (nota >=80) {
+    console.log("B");
+} else if (nota >= 70) {
+    console.log("C");
+} else if (nota >= 60) {
+    console.log("D");
+} else if (nota >=50){
+    console.log("E");
+} else if (nota < 0 || nota > 100){
+    console.log("Erro");
+} else {
+    console.log("F");
+}
+
 
