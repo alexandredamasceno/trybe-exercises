53625820eda2b113ffc1dfdcc87849d6118809f6
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-02-24 23:19:01 -0300

    Segundo requisito

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_1.js
@@ -13,3 +13,16 @@ console.log("- Divisão")
 console.log(valorA / valorB)
 console.log("- Módulo")
 console.log(((valorA + valorB) %2) == 0)
+
+console.log("Segundo Requisito:")
+
+let num1 = 10;
+let num2 = 20;
+
+if (num1 > num2) {
+    console.log("Maior número:")
+    console.log(num1);
+} else {
+    console.log("Maior número:")
+    console.log(num2);
+}
