1b7a3c8e04af2fbc98a322d0d0e7273078ee253a
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-02-24 23:10:09 -0300

    Primeiro requisito

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_1.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!")
+
+let valorA = 10;
+let valorB = 5;
+
+console.log("- Adição")
+console.log(valorA + valorB)
+console.log("- Subtração")
+console.log(valorA - valorB)
+console.log("- Multiplicação")
+console.log(valorA * valorB)
+console.log("- Divisão")
+console.log(valorA / valorB)
+console.log("- Módulo")
+console.log(((valorA + valorB) %2) == 0)
