b1c32e9cf698577cedd7b70ac190c7908e409829
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-02-26 00:30:16 -0300

    Terceira requisito

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_2.js
@@ -12,3 +12,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = numbers.reduce((acumulador, elementoatual) => acumulador +elementoatual);
 console.log("Soma total= "+soma);
 
+console.log("Terceiro Requisito");
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let total = numbers.reduce((acumulador, elementoatual) => acumulador +elementoatual);
+
+
+let mediaAritmetica = total / numbers.length;
+
+console.log(mediaAritmetica);
