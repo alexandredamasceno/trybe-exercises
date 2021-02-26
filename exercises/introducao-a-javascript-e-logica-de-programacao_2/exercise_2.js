58da0047d8b0c987f72a3b93e1c6bb3100ec1bb9
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-02-26 00:18:13 -0300

    Segundo requisito

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_2.js
@@ -3,3 +3,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index +=1) {
     console.log(numbers[index]);
 }
+
+console.log("Segundo Requisito");
+// No começo eu tentei resolver por meio de um laço for, porém, não deu muito certo. Ai fui pesquisar a respeito, até que encontrei um artigo nesse site: https://medium.com/@raullesteves/javascript-entendendo-o-reduce-de-uma-vez-por-todas-c4cbaa16e380 e resolveu meu problema. Eu me atentei ao báscio, abstrai o necessário e o resultado é esse que você pode ver abaixo.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = numbers.reduce((acumulador, elementoatual) => acumulador +elementoatual);
+console.log("Soma total= "+soma);
+
