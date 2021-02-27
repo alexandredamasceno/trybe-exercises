6dc83f46309d4619064d88983ae56fe8c96a451f
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-02-27 14:45:42 -0300

    Segundo Item

--- exercises/introducao-a-javascript-e-logica-de-programacao.3/script_3.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao.3/script_3.js
@@ -11,3 +11,13 @@ for (let index = 1; index < n; index +=1) {
   console.log(quadradoAsterisco);
 }
 
+console.log("Segundo Item");
+// Mais uma vez eu busquei orientação no gabarito do Course.
+let n2 = 6;
+let asterisco = "*";
+let triangulo = "";
+
+for (let indexRet = 1; indexRet <=n2; indexRet +=1){
+  console.log(triangulo);
+  triangulo = triangulo + asterisco;
+}
