5eb74c97abf90516d6404468debaf0cf05c055c8
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-02-27 14:33:48 -0300

    Primeiro Item

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao.3/script_3.js
@@ -0,0 +1,13 @@
+console.log("Primeiro Item")
+// Busquei orientação no próprio gabarito do Course.
+let n = 6;
+let asterisco = "*";
+let quadradoAsterisco = "";
+
+for (let index = 1; index < n; index +=1){
+  quadradoAsterisco = quadradoAsterisco + asterisco;
+}
+for (let index = 1; index < n; index +=1) {
+  console.log(quadradoAsterisco);
+}
+
