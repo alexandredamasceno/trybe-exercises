c97a99750612d4c675b5b38408f73948c74badeb
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-02-27 15:14:57 -0300

    Terceiro Item

--- exercises/introducao-a-javascript-e-logica-de-programacao.3/script_3.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao.3/script_3.js
@@ -21,3 +21,31 @@ for (let indexRet = 1; indexRet <=n2; indexRet +=1){
   console.log(triangulo);
   triangulo = triangulo + asterisco;
 }
+
+console.log("Terceiro Item")
+
+let n4 = 6;
+let asterisco2 = "*";
+let triangulo2 = "";
+
+for (let index3 = 1; index3 <= n4; index3 +=1){
+  if (index3 == 1){
+  console.log("     "+ triangulo2);
+  triangulo2 = triangulo2 + asterisco2;
+} else if (index3 == 2){
+  console.log("    "+ triangulo2);
+  triangulo2 = triangulo2 + asterisco2;
+} else if (index3 == 3){
+  console.log("   " + triangulo2);
+  triangulo2 = triangulo2 + asterisco2;
+} else if (index3 == 4){
+  console.log("  " + triangulo2);
+  triangulo2 = triangulo2 + asterisco2;
+} else if (index3 == 5){
+  console.log(" " + triangulo2);
+  triangulo2 = triangulo2 + asterisco2;
+} else if (index3 == 6){
+  console.log("" + triangulo2);
+  triangulo2 = triangulo2 + asterisco2;
+} 
+}
