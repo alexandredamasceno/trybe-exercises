a8bc42ee16bf945b1bc04c03a8b59ad4e0b35e47
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-01 22:57:29 -0300

    quinto item

--- exercises/introducao-a-javascript-e-logica-de-programacao.4/script_4.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao.4/script_4.js
@@ -23,3 +23,14 @@ let info = {
   for (let key in info) {
       console.log(info[key])
   }
+
+  console.log("Quinto item");
+
+  let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim",
+  };
+
+  console.log(info.personagem+" e "+info2.personagem+": "+info.origem+" e "+info2.origem+" "+info.nota+" e "+info2.nota+" ambos recorrentes.")
