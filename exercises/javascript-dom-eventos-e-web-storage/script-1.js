6f560315c1387139f2ac395e6c263fa01eaa0fc3
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-04 16:03:19 -0300

    Monta a estrutura do último requisito do exercício, mas não finaliza

--- exercises/javascript-dom-eventos-e-web-storage/script-1.js
+++ exercises/javascript-dom-eventos-e-web-storage/script-1.js
@@ -2,21 +2,43 @@ function changeText (text){
     document.querySelectorAll(".center-content p")[0].innerText = text;
 }
 
-changeText("Como um excelente programador, comprometido, dedicado e SEMPRE CURIOSO, nada acomodado, dando o meu melhor sempre.")
+changeText("Como um excelente programador, comprometido, dedicado e SEMPRE CURIOSO, nada acomodado, dando o meu melhor sempre.");
 
 function changesColor (color){
     document.querySelector("div").style.backgroundColor = color;
 }
 
-changesColor("rgb(76,164,109)")
+changesColor("rgb(76,164,109)");
 
 function changesColorSquare (color){
     document.getElementsByClassName("center-content")[0].style.backgroundColor = color;
 }
-changesColorSquare("white")
+changesColorSquare("white");
 
 function fixText (text){
     document.querySelector("h1").innerText = text;
 }
 
-fixText ("Exercício 5.1 - JavaScript")
+fixText ("Exercício 5.1 - JavaScript");
+
+function textUppercase (){
+    document.querySelectorAll("p")[0].style.textTransform= "uppercase";
+}
+
+textUppercase();
+
+function showTextConsole (tag){
+
+    let retorno = document.getElementsByTagName(tag);
+    for (let i in tag){
+        if (tag[i] == [0]){
+            console.log(tag[0].innerText);
+        }else if (tag[i] == [1]){
+            console.log(tag[1].innerText);
+        }else if (tag[i] == [2]){
+            console.log(tag[2].innerText)
+        }
+    }
+}
+showTextConsole("p")
+
