16469cb51bd9903ff008c1b547a6dc7d9dfb9996
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-04 10:02:10 -0300

    Segundo requisito - exercícios 5.1

--- exercises/javascript-dom-eventos-e-web-storage/script-1.js
+++ exercises/javascript-dom-eventos-e-web-storage/script-1.js
@@ -1,5 +1,11 @@
 function changeText (text){
-    document.querySelectorAll(".center-content p")[0].innerText = text
+    document.querySelectorAll(".center-content p")[0].innerText = text;
 }
 
 changeText("Como um excelente programador, comprometido, dedicado e SEMPRE CURIOSO, nada acomodado, dando o meu melhor sempre.")
+
+function changesColor (color){
+    document.querySelector("div").style.backgroundColor = color;
+}
+
+changesColor("rgb(76,164,109)")
