cd0cc0a70e8098b6f380f2c9e733362c557e358e
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-04 09:21:11 -0300

    Finaliza exercício da parte III do conteúdo

--- exercises/javascript-dom-eventos-e-web-storage/script.js
+++ exercises/javascript-dom-eventos-e-web-storage/script.js
@@ -14,3 +14,6 @@ document.querySelectorAll("section")[1].style.backgroundColor ="rgb(255,255,0)";
 
 //Footer
 document.getElementsByTagName("footer")[0].style.backgroundColor = "rgb(47,79,79)"
+
+
+
