8e417e17c14de4ed04d446418788bbd11f9bceec
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-04 09:56:50 -0300

    Primeiro requisito - exercícios 5.1

---
+++ exercises/javascript-dom-eventos-e-web-storage/script-1.js
@@ -0,0 +1,5 @@
+function changeText (text){
+    document.querySelectorAll(".center-content p")[0].innerText = text
+}
+
+changeText("Como um excelente programador, comprometido, dedicado e SEMPRE CURIOSO, nada acomodado, dando o meu melhor sempre.")
