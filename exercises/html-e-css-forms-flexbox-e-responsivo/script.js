2ae78e0759d73cd468a91aac52fa2cb57de36886
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-15 21:59:18 -0300

    Requisito 1-completo

---
+++ exercises/html-e-css-forms-flexbox-e-responsivo/script.js
@@ -0,0 +1,19 @@
+let estadoPai = document.getElementById("input-estado");
+let estadosBr = [ 'AC - Acre', 'AL - Alagoas', 'AP - Amapá', 'AM - Amazonas', 'BA - Bahia', 'CE - Ceará', 'DF - Distrito Federal', 'ES - Espírito Santo', 'GO - Goías', 'MA - Maranhão', 'MT - Mato Grosso', 'MS - Mato Grosso do Sul', 'MG - Minas Gerais', 'PA - Pará', 'PB - Paraíba', 'PR - Paraná', 'PE - Pernambuco', 'PI - Piauí', 'RJ - Rio de Janeiro', 'RN - Rio Grande do Norte', 'RS - Rio Grande do Sul', 'RO - Rondônia', 'RR - Roraíma', 'SC - Santa Catarina', 'SP - São Paulo', 'SE - Sergipe', 'TO - Tocantins' ];
+let gurdaNumero = [];
+for (let i = 0; i < 27; i += 1){
+    let Numero = [i];
+    gurdaNumero.push(Numero);
+}
+
+
+for (let i = 0; i < estadosBr.length; i += 1){
+    let guardaEstado = estadosBr[i];
+    let numeroEstado = gurdaNumero[i];
+    let criaEstados = document.createElement("option");
+    criaEstados.innerHTML = guardaEstado;
+    criaEstados.id = "estado - " + numeroEstado;
+    
+    estadoPai.appendChild(criaEstados)
+}
+
