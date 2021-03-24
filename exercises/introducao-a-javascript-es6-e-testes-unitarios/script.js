a9b8dc445d3ec892d7ef409398b417e6afc2de47
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-23 23:04:22 -0300

    Requisito 2 - Parte I

--- exercises/introducao-a-javascript-es6-e-testes-unitarios/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios/script.js
@@ -11,3 +11,25 @@ const testingScope = (escopo) => {
 
   testingScope(true);
   testingScope(false);
+
+  //Requisito 2
+// Busquei orientação no site https://www.w3schools.com/jsref/jsref_sort.asp. Sintaxe: array.sort(compareFunction)
+// Quando o método sort () compara dois valores, ele envia os valores para a função de comparação e os classifica de acordo com o valor retornado (negativo, zero, positivo). Exemplo : Ao comparar 40 e 100, o método sort () chama a função compare (40,100). A função calcula 40-100 e retorna -60 (um valor negativo). A função de classificação classificará 40 como um valor inferior a 100.
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+    //funcção anônima:
+  oddsAndEvens.sort(function(a, b){return a-b})
+  console.log(`Os números ${oddsAndEvens} se encontram de forma crescente!`);
+
+    //funcção nominal:
+  function ordenaArray(a, b){
+    return a - b;
+  }
+
+  console.log(`Os números ${oddsAndEvens.sort(ordenaArray)} se encontram de forma crescente!`);
+
+  //Outro exemplo com texto:
+  let fruit = ['cherries', 'apples', 'bananas']
+
+  console.log(fruit.sort())
+
