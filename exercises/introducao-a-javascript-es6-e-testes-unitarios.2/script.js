03a1e37c7378a8fd1f3c649c38a0856358eb1fa0
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-25 02:28:32 -0300

    Parte II - itens de 1 a 5

--- exercises/introducao-a-javascript-es6-e-testes-unitarios.2/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios.2/script.js
@@ -55,3 +55,81 @@ const orderModifier = (order) => {
 }
 
 orderModifier(order);
+
+//Parte II
+//Item 1:
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  function adicionaNewTurno(objeto, chave, valor) {
+    objeto[chave] = valor;
+  }
+
+  adicionaNewTurno(lesson2, 'turno', 'manhã');
+  console.log(lesson2);
+
+  //Item 2:
+  function listaKeys(objeto) {
+    const array = Object.keys(objeto);
+    for (let i in array){
+        console.log('-----------');
+        console.log(array[i]);
+    }
+  }
+  listaKeys(lesson1);
+
+  //Item 3:
+  function sizeObj(objeto) {
+    const obj = Object.keys(objeto).length;
+    console.log(`Size = ${obj}`);
+  }
+  sizeObj(lesson1);
+
+  // Item 4:
+  function listaValues(objeto) {
+      const valores = Object.values(objeto);
+      for (let i in valores){
+          console.log(`Valores = ${valores[i]}`);
+      }
+  }
+  listaValues(lesson1);
+
+  //Item 5:
+  const allLessons = Object.assign({}, {lesson1}, {lesson2}, {lesson3});
+
+  console.log(allLessons);
+
+  function totalStudents() {
+    const alunos1 = allLessons.lesson1.numeroEstudantes;
+    const alunos2 = allLessons.lesson2.numeroEstudantes;
+    const alunos3 = allLessons.lesson3.numeroEstudantes;
+    
+    console.log(alunos1);
+    console.log(alunos2);
+    console.log(alunos3); 
+    
+    const total = alunos1 + alunos2 + alunos3;
+    console.log(total);
+  }
+  totalStudents();
+
+
+  
+  
