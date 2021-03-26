05948c62a02fa77aaf8789304159f266d4b5a41d
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-25 23:44:06 -0300

    Funcao 4

--- exercises/introducao-a-javascript-es6-e-teste-unitarios.3/script.js
+++ exercises/introducao-a-javascript-es6-e-teste-unitarios.3/script.js
@@ -62,3 +62,19 @@ myRemoveWithoutCopy(meuArray, 2);
 assert.notDeepStrictEqual(meuArray, [6, 7, 2, 9]);
 assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
 
+//Função 4:
+const assert = require('assert');
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+assert.strictEqual(myFizzBuzz(15), 'fizzbuzz')
+assert.strictEqual(myFizzBuzz(9), 'fizz')
+assert.strictEqual(myFizzBuzz(5), 'buzz')
+assert.notStrictEqual(myFizzBuzz(7), 'fizzbuzz')
+assert.strictEqual(myFizzBuzz('9'), false);
