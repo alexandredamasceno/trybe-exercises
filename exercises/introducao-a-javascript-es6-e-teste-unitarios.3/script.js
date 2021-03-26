3f4d235b5ede1eee28349b7a872254c0fbb36cc9
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-25 22:35:41 -0300

    Funcao 2

--- exercises/introducao-a-javascript-es6-e-teste-unitarios.3/script.js
+++ exercises/introducao-a-javascript-es6-e-teste-unitarios.3/script.js
@@ -14,3 +14,26 @@ assert.strictEqual(sum(0, 0), 0);
 assert.throws(()=>{sum(4, '5'), 9});
 assert.strictEqual(sum(4, '5'), 9);
 
+
+// Função 2:
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
+const newArray = [8, 9, 7, 3];
+myRemove(newArray, 9);
+assert.deepStrictEqual(newArray, [8, 9, 7,3]);
