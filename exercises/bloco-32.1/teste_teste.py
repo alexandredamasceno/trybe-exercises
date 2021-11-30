2fc621445d6066eded812d6e5fde26d16bbacf5f
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-11-30 18:26:02 -0300

    Exercise 2

--- exercises/bloco-32.1/teste_teste.py
+++ exercises/bloco-32.1/teste_teste.py
@@ -6,3 +6,15 @@ def big_number(num1, num2):
 
 
 print(big_number(4, 6))
+
+# Ex 2:
+
+
+def media(list):
+    sum = 0
+    for item in list:
+        sum += item
+    return sum / len(list)
+
+
+print(media([1, 2, 3, 4, 5]))
